781b54987ada764c51eb5efb8f26ad200c2e34df
NaranjaCrocante <j_camilo27@hotmail.com>
2026-02-01 10:50:42 -0300

    actualizacion
    1.0.3

--- mercado.js
+++ mercado.js
@@ -1,1 +1,10 @@
-alert("¡Bienvenido al Mercado de La Taberna!")
+// alert("¡Bienvenido al Mercado de La Taberna!")
+
+const viewportheight = window.innerHeight;
+const element = document.querySelector('#inicio');
+
+function caidaCartel(){
+    if(window.scrollY >= viewportheight/2){
+        element.classList.add("animate__animated","animate__jackInTheBox");
+    } 
+} 
